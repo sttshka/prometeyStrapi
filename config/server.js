1cff398e4bede6f2801d84b3a77f2482655f7686
Andrew Peshko <apeshko@metaprime.ru>
2022-09-09 20:47:28 +0300

    add app-keys

--- config/server.js
+++ config/server.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://allsport29.store',
+  url: 'https://allsport29.ru/admin',
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
 });
